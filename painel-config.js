75b1719cbf37f3cff2e5740b9fe9dd0dccbee2ac
Manoel Santos <127696862+Manu-embu@users.noreply.github.com>
2026-09-06 15:20:09 -0300

    v0.5.6 - integra Painel da Demanda ao portal público

---
+++ painel-config.js
@@ -0,0 +1,3 @@
+// URL do Web App administrativo do Painel da Demanda.
+// Preencha após implantar o projeto em 03_PAINEL_DEMANDA_APPS_SCRIPT.
+window.PAINEL_DEMANDA_URL = 'https://script.google.com/a/macros/educacaoembudasartes.com/s/AKfycbzbozxcPFJat_KafpSIzvD928n8T6hDFD43mhFNNXLoiFCJuHCLFhz3-Q1yWARcW7dVQw/exec';
